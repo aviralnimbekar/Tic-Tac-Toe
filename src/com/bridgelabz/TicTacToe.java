e4b1c7aa54941fd59bdc19094e610a0ae4ea31f1
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-30 17:25:50 +0530

    ADDED : Empty board cells

---
+++ src/com/bridgelabz/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+    }
+
+    public static void initialisingBoard() {
+        char[] emptyBoardCells = new char[10];
+        for (int i = 0; i < 10; i++) {
+            emptyBoardCells[i] = ' ';
+        }
+    }
+}
